472a701e6ab9dcadb35d888d03f151e2d7920836
tanishakhoria <khoriatanisha@gmail.com>
2026-05-31 15:45:34 +0530

    Added real file hashing and blob storage

--- backend/main.go
+++ backend/main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"rewindfs/internal/storage"
 )
 
 func main() {
 
-	content := []byte("Hello RewindFS")
+	content, err := os.ReadFile("text.txt")
+	if err != nil {
+		fmt.Println("Read Error:", err)
+		return
+	}
 
 	hash := storage.GenerateHash(content)
 
+	fmt.Println("File Content:", string(content))
 	fmt.Println("Hash:", hash)
 
-	err := storage.SaveBlob(hash, content)
+	err = storage.SaveBlob(hash, content)
 
 	if err != nil {
 		fmt.Println("Blob Error:", err)
