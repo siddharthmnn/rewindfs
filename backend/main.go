216a7b6bee02d16b0bf8215c1792751d324f5cdf
tanishakhoria <khoriatanisha@gmail.com>
2026-06-01 12:26:18 +0530

    Added restore by snapshot ID

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"rewindfs/internal/storage"
 )
 
 func main() {
 
-	hash := "fa8efa7b40ee2b2bf031f4790373b12586069e4572807cbecede997723798ac5"
+	// Step 1: Read file content
+	content, err := os.ReadFile("text.txt")
+	if err != nil {
+		fmt.Println("Read Error:", err)
+		return
+	}
 
-	err := storage.RestoreFile(hash, "restored.txt")
+	// Step 2: Generate hash
+	hash := storage.GenerateHash(content)
 
+	// Step 3: Print file content and hash
+	fmt.Println("File Content:", string(content))
+	fmt.Println("Hash:", hash)
+
+	// Step 4: Save blob
+	err = storage.SaveBlob(hash, content)
+	if err != nil {
+		fmt.Println("Blob Error:", err)
+		return
+	}
+	fmt.Println("Blob Saved Successfully!")
+
+	// Step 5: Restore from blob
+	err = storage.RestoreFile(hash, "restored.txt")
 	if err != nil {
 		fmt.Println("Restore Error:", err)
 		return
