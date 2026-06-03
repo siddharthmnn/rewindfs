76de11757544c89a00254f64206196f3443c3dff
Siddharth S Menon <siddharthmenon04@gmail.com>
2026-06-03 16:10:04 +0000

    Add snapshot content viewer dashboard

--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"rewindfs/internal/diff"
-)
+import "rewindfs/internal/api"
 
 func main() {
-
-	oldData, err := os.ReadFile("old.txt")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	newData, err := os.ReadFile("new.txt")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	result := diff.Compare(
-		string(oldData),
-		string(newData),
-	)
-
-	fmt.Println(result)
+	api.StartServer()
 }
